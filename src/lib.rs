6582044ee79ec9d714797dd48d6b5c934c69813d
David Podhola <david.podhola@hsharp.software>
2017-09-07 20:04:10 +0200

    buildable

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,44 @@ extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
 
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
 
 use std::io::{self, Write};
 use futures::{Future, Stream};
-use hyper::Client;
 use tokio_core::reactor::Core;
+use hyper::{Client, Chunk, Method, Request};
+use hyper::header::{ContentLength, ContentType};
+
+fn post<T>(body: String, parser: fn(Chunk) -> Option<T> )  -> Option<T>
+    where T: serde::Serialize
+{
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    let uri = "http://httpbin.org/post".parse().unwrap();
+
+    let mut req = Request::new(Method::Post, uri);
+    req.set_body(body);
+
+    let mut result : Option<T> = None;
+    {
+        let post = client.request(req).and_then(|res| {
+            res.body().concat2().and_then(|body| {
+                //let v: T = serde_json::from_slice(&body).unwrap();
+                result = parser(body);
+                Ok(())
+            })
+        });    
+
+        core.run(post).unwrap();
+    }
+
+    result
+}
 
 #[cfg(test)]
 mod tests {
