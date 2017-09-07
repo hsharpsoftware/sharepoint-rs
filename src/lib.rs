eca0139624c2359435dabd9950271e3c11cea5fe
David Podhola <david.podhola@hsharp.software>
2017-09-07 20:18:13 +0200

    test started

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,13 @@ use tokio_core::reactor::Core;
 use hyper::{Client, Chunk, Method, Request};
 use hyper::header::{ContentLength, ContentType};
 
-fn post<T>(body: String, parser: fn(Chunk) -> Option<T> )  -> Option<T>
+fn post<T>(url: String, body: String, parser: fn(Chunk) -> Option<T> )  -> Option<T>
     where T: serde::Serialize
 {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
 
-    let uri = "http://httpbin.org/post".parse().unwrap();
+    let uri = url.parse().unwrap();
 
     let mut req = Request::new(Method::Post, uri);
     req.set_body(body);
@@ -43,7 +43,18 @@ fn post<T>(body: String, parser: fn(Chunk) -> Option<T> )  -> Option<T>
 
 #[cfg(test)]
 mod tests {
+    use serde_json::Value;
+    use hyper::{Chunk};
+    use super::*;
+
+    fn parse_json( body : Chunk ) -> Option<Value> {
+        let v: Value = serde_json::from_slice(&body).unwrap();
+        Some(v)
+    }
+
     #[test]
     fn it_works() {
+        let res = post("http://httpbin.org/post".to_string(), "".to_string(), parse_json);
+        println!("Got '{:?}'", res);
     }
 }
