ab0e1c1bf258eaf1a5725588432a43bf9d423b9f
David Podhola <david.podhola@hsharp.software>
2017-09-07 18:33:44 +0200

    crates ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use hyper::Client;
+use tokio_core::reactor::Core;
+
 #[cfg(test)]
 mod tests {
     #[test]
